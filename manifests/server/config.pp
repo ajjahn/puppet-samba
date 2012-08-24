0ac18d517632fdf59f83e7184b8dd35a99909c30
Adam Jahn <ajjahn@gmail.com>
2012-08-24 11:32:00 -0400

    var isn't needed in server config, but it's nice to require the config dir, and permisions

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -1,11 +1,19 @@
 class samba::server::config {
-  
+
+  file { "/etc/samba":
+    ensure => directory,
+    owner => "root",
+    group => "root",
+    mode => 0755,
+  }
+
   file { "/etc/samba/smb.conf":
-    ensure => $ensure,
-    owner => root,
-    group => root,
-    require => Class["samba::server::install"],
+    ensure => present,
+    owner => "root",
+    group => "root",
+    mode => 0644,
+    require => [File["/etc/samba"], Class["samba::server::install"]],
     notify => Class["samba::server::service"]
   }
-  
+
 }
