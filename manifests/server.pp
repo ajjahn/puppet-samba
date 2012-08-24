7c10f82e1bf5e0d79283efdcffe154f3411486ee
Adam Jahn <ajjahn@gmail.com>
2012-08-24 12:27:26 -0400

    workgroup should be optional and generic

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,16 +1,24 @@
-class samba::server($workgroup = PCCH) {
+class samba::server($workgroup = '') {
 	include samba::server::install
 	include samba::server::config
 	include samba::server::service
 
+  $context = "/files/etc/samba/smb.conf"
   $target = "target[. = 'global']"
 
-  augeas { global:
-    context => "/files/etc/samba/smb.conf",
-    changes => [
-      "set ${target} global",
-      "set ${target}/workgroup $workgroup"
-    ],
+  augeas { 'global-section':
+    context => $context,
+    changes => "set ${target} global",
     require => Class["samba::server::config"]
   }
+
+  augeas { 'global-workgroup':
+    context => $context,
+    changes => $workgroup ? {
+      default => "set ${target}/workgroup $workgroup",
+      '' => "rm ${target}/workgroup",
+    },
+    require => Augeas['global-section'],
+  }
+
 }
