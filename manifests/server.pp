9e16baf39baffc185f57df0227b0d3a314e50990
Adam Jahn <ajjahn@gmail.com>
2012-08-24 11:57:15 -0400

    ensure global section exists in case of empty config file

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,12 +2,15 @@ class samba::server($workgroup = PCCH) {
 	include samba::server::install
 	include samba::server::config
 	include samba::server::service
-  
+
+  $target = "target[. = 'global']"
+
   augeas { global:
     context => "/files/etc/samba/smb.conf",
     changes => [
-      "set target[. = 'global']/workgroup $workgroup"
-      ],
+      "set ${target} global",
+      "set ${target}/workgroup $workgroup"
+    ],
     require => Class["samba::server::config"]
   }
 }
