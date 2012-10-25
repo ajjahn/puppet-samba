06c07f98c82ca7582e1c76b19fcf0a8f55727267
Adam Jahn <ajjahn@gmail.com>
2012-10-25 00:38:37 -0400

    lint test share file

--- manifests/server/share.pp
+++ manifests/server/share.pp
@@ -14,7 +14,7 @@ define samba::server::share($ensure = present,
                             $path = '',
                             $read_only = '') {
 
-  $context = "/files/etc/samba/smb.conf"
+  $context = '/files/etc/samba/smb.conf'
   $target = "target[. = '${name}']"
 
   augeas { "${name}-section":
@@ -23,153 +23,153 @@ define samba::server::share($ensure = present,
       present => "set ${target} '${name}'",
       default => "rm ${target} '${name}'",
     },
-    require => Class["samba::server::config"],
-    notify => Class["samba::server::service"]
+    require => Class['samba::server::config'],
+    notify  => Class['samba::server::service']
   }
 
-  if $ensure == "present" {
+  if $ensure == 'present' {
     augeas { "${name}-browsable":
       context => $context,
       changes => $browsable ? {
-        true => "set ${target}/browsable yes",
-        false => "set ${target}/browsable no",
+        true    => "set ${target}/browsable yes",
+        false   => "set ${target}/browsable no",
         default => "rm ${target}/browsable",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-comment":
       context => $context,
       changes => $comment ? {
         default => "set ${target}/comment '${comment}'",
-        '' => "rm ${target}/comment",
+        ''      => "rm ${target}/comment",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-copy":
       context => $context,
       changes => $copy ? {
         default => "set ${target}/copy '${copy}'",
-        '' => "rm ${target}/copy",
+        ''      => "rm ${target}/copy",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-create_mask":
       context => $context,
       changes => $create_mask ? {
         default => "set \"${target}/create mask\" '${create_mask}'",
-        '' => "rm \"${target}/create mask\"",
+        ''      => "rm \"${target}/create mask\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-directory_mask":
       context => $context,
       changes => $directory_mask ? {
         default => "set \"${target}/directory mask\" '${directory_mask}'",
-        '' => "rm \"${target}/directory mask\"",
+        ''      => "rm \"${target}/directory mask\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-force_create_mask":
       context => $context,
       changes => $force_create_mask ? {
         default => "set \"${target}/force create mask\" '${force_create_mask}'",
-        '' => "rm \"${target}/force create mask\"",
+        ''      => "rm \"${target}/force create mask\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-force_directory_mask":
       context => $context,
       changes => $force_directory_mask ? {
         default => "set \"${target}/force directory mask\" '${force_directory_mask}'",
-        '' => "rm \"${target}/force directory mask\"",
+        ''      => "rm \"${target}/force directory mask\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-force_group":
       context => $context,
       changes => $force_group ? {
         default => "set \"${target}/force group\" '${force_group}'",
-        '' => "rm \"${target}/force group\"",
+        ''      => "rm \"${target}/force group\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-force_user":
       context => $context,
       changes => $force_user ? {
         default => "set \"${target}/force user\" '${force_user}'",
-        '' => "rm \"${target}/force user\"",
+        ''      => "rm \"${target}/force user\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-guest_account":
       context => $context,
       changes => $guest_account ? {
         default => "set \"${target}/guest account\" '${guest_account}'",
-        '' => "rm \"${target}/guest account\"",
+        ''      => "rm \"${target}/guest account\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-guest_ok":
       context => $context,
       changes => $guest_ok ? {
-        true => "set \"${target}/guest ok\" yes",
-        false => "set \"${target}/guest ok\" no",
+        true    => "set \"${target}/guest ok\" yes",
+        false   => "set \"${target}/guest ok\" no",
         default => "rm \"${target}/guest ok\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-guest_only":
       context => $context,
       changes => $guest_only ? {
-        true => "set \"${target}/guest only\" yes",
-        false => "set \"${target}/guest only\" no",
+        true    => "set \"${target}/guest only\" yes",
+        false   => "set \"${target}/guest only\" no",
         default => "rm \"${target}/guest only\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-path":
       context => $context,
       changes => $path ? {
         default => "set ${target}/path '${path}'",
-        '' => "rm ${target}/path",
+        ''      => "rm ${target}/path",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
 
     augeas { "${name}-read_only":
       context => $context,
       changes => $read_only ? {
-        true => "set \"${target}/read only\" yes",
-        false => "set \"${target}/read only\" no",
+        true    => "set \"${target}/read only\" yes",
+        false   => "set \"${target}/read only\" no",
         default => "rm \"${target}/read_only\"",
       },
       require => Augeas["${name}-section"],
-      notify => Class["samba::server::service"]
+      notify  => Class['samba::server::service']
     }
   }
 }
