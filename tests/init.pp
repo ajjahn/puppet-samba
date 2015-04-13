40e085df33b16568490c3b7ee893f8abce10abf1
Adam Jahn <ajjahn@gmail.com>
2015-04-12 22:50:00 -0400

    remove superfluous curly brace in smoke test

--- tests/init.pp
+++ tests/init.pp
@@ -7,19 +7,19 @@ class {'samba::server':
     security      => 'share'
   }
 
-  samba::server::share {'example-share':
-    comment              => 'Example Share',
-    path                 => '/path/to/share',
-    guest_only           => true,
-    guest_ok             => true,
-    guest_account        => "guest",
-    browsable            => false,
-    create_mask          => 0777,
-    force_create_mask    => 0777,
-    directory_mask       => 0777,
-    force_directory_mask => 0777,
-    force_group          => 'group',
-    force_user           => 'user',
-    copy                 => 'some-other-share',
-  }
+samba::server::share {'example-share':
+  comment              => 'Example Share',
+  path                 => '/path/to/share',
+  guest_only           => true,
+  guest_ok             => true,
+  guest_account        => "guest",
+  browsable            => false,
+  create_mask          => 0777,
+  force_create_mask    => 0777,
+  directory_mask       => 0777,
+  force_directory_mask => 0777,
+  force_group          => 'group',
+  force_user           => 'user',
+  copy                 => 'some-other-share',
 }
+
