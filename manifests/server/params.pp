8e332dafba6c6a1975d942801c98467723988fb7
Adam Jahn <ajjahn@gmail.com>
2015-04-13 15:14:36 -0400

    extract os params to params class

---
+++ manifests/server/params.pp
@@ -0,0 +1,30 @@
+# == Class samba::server::params
+#
+class samba::server::params {
+  case $::osfamily {
+    'Redhat': { $service_name = 'smb' }
+
+    #On Debian family: Debian 7 => samba , Ubuntu => smbd
+    #Others, I don't know, hope 'samba' will works
+    'Debian': {
+      case $::operatingsystem{
+        'Debian': { $service_name = 'samba' }
+        'Ubuntu': { $service_name = 'smbd' }
+        default: { $service_name = 'samba' }
+      }
+    }
+    'Gentoo': { $service_name = 'samba' }
+    'Archlinux': { $service_name = 'smbd' }
+
+    # Currently Gentoo has $::osfamily = "Linux". This should change in
+    # Factor 1.7.0 <http://projects.puppetlabs.com/issues/17029>, so
+    # adding workaround.
+    'Linux': {
+      case $::operatingsystem {
+        'Gentoo':  { $service_name = 'samba' }
+        default: { fail("${::operatingsystem} is not supported by this module.") }
+      }
+    }
+    default: { fail("${::osfamily} is not supported by this module.") }
+  }
+}
--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -1,32 +1,9 @@
-class samba::server::service ($ensure = running, $enable = true) {
-  case $::osfamily {
-      'Redhat': { $service_name = 'smb' }
-
-      #On Debian family: Debian 7 => samba , Ubuntu => smbd
-      #Others, I don't know, hope 'samba' will works
-      'Debian': {
-        case $::operatingsystem{
-                'Debian': { $service_name = 'samba' }
-                'Ubuntu': { $service_name = 'smbd' }
-                default: { $service_name = 'samba' }
-        }
-      }
-      'Gentoo': { $service_name = 'samba' }
-      'Archlinux': { $service_name = 'smbd' }
-
-      # Currently Gentoo has $::osfamily = "Linux". This should change in
-      # Factor 1.7.0 <http://projects.puppetlabs.com/issues/17029>, so
-      # adding workaround.
-      'Linux': {
-        case $::operatingsystem {
-          'Gentoo':  { $service_name = 'samba' }
-          default: { fail("${::operatingsystem} is not supported by this module.") }
-        }
-      }
-      default: { fail("${::osfamily} is not supported by this module.") }
-    }
 # == Class samba::server::server
 #
+class samba::server::service (
+  $ensure = running,
+  $enable = true
+) inherits samba::server::params {
 
   service { $service_name :
     ensure      => $ensure,
@@ -35,5 +12,4 @@ class samba::server::service ($ensure = running, $enable = true) {
     enable      => $enable,
     require     => Class['samba::server::config']
   }
-
 }
