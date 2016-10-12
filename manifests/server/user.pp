9715085175447f880129e086471bec305d1f6779
Oliver Bertuch <poikilotherm@users.noreply.github.com>
2016-10-12 14:54:30 +0200

    Better check if password is set for user
    This is grabbed from https://github.com/h0tbird/puppet-samba/blob/master/manifests/user.pp, so credits to @h0tbird.

--- manifests/server/user.pp
+++ manifests/server/user.pp
@@ -8,7 +8,7 @@ define samba::server::user (
 
   exec { "add smb account for ${user_name}":
     command => "/bin/echo -e '${password}\\n${password}\\n' | /usr/bin/pdbedit --password-from-stdin -a '${user_name}'",
-    unless  => "/usr/bin/pdbedit '${user_name}'",
+    unless  => "printf ${password} | iconv -f ASCII -t UTF-16LE | openssl md4 | awk '{print \"pdbedit -wL ${user_name} | grep -qi \"\$2}' | sh",
     require => [ User[$user_name] ],
     notify  => Class['samba::server::service'] #TODO: Is this really required??
   }
