a040c6e4e2f01661de6e53e6d120bae0eef18c5b
Oliver Bertuch <poikilotherm@users.noreply.github.com>
2016-10-12 14:57:35 +0200

    Update spec test for samba::server::user

--- spec/defines/samba__server__user_spec.rb
+++ spec/defines/samba__server__user_spec.rb
@@ -7,7 +7,7 @@ describe 'samba::server::user', :type => :define do
   it { is_expected.to contain_samba__server__user('test_user') }
   it { is_expected.to contain_exec('add smb account for test_user').with(
     :command => '/bin/echo -e \'secret\nsecret\n\' | /usr/bin/pdbedit --password-from-stdin -a \'test_user\'',
-    :unless  => '/usr/bin/pdbedit \'test_user\'',
+    :unless  => 'printf secret | iconv -f ASCII -t UTF-16LE | openssl md4 | awk '{print \"pdbedit -wL test_user | grep -qi \"\$2}' | sh',
     :require => 'User[test_user]',
     :notify  => 'Class[Samba::Server::Service]'
   ) }
